ad01e91cb4760226ee10f8e05d42b512611045dd
didikee <didikee@qq.com>
2024-12-21 00:59:10 +0800

    upgrade to androidx

--- indicatorseekbar/src/main/java/com/warkiz/widget/IndicatorSeekBar.java
+++ indicatorseekbar/src/main/java/com/warkiz/widget/IndicatorSeekBar.java
@@ -15,9 +15,6 @@ import android.graphics.drawable.StateListDrawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Parcelable;
-import android.support.annotation.ColorInt;
-import android.support.annotation.NonNull;
-import android.support.annotation.RequiresApi;
 import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
@@ -27,6 +24,10 @@ import android.view.ViewParent;
 import android.view.WindowManager;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
+
+import androidx.annotation.ColorInt;
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
